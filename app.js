edca654920b084e3ab9cd8e2832ca5aaf978b341
Nithishmaduraiveeran <139096677+Nithishmaduraiveeran@users.noreply.github.com>
2023-07-13 03:29:06 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_bin=document.getElementById("ON");
+var on_bin=document.getElementById("OFF");
+var img=document.getElementById("bulb");
+
+function bulb_ON(){
+    img.src="./download.jfif"
+}
+
+function bulb_OFF(){
+    img.src="./download (2).jfif"
+}
